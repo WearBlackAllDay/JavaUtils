b0a50545de06e0102d33a6f941b106c5ef24726f
Lara <69314462+WearBlackAllDay@users.noreply.github.com>
2020-09-29 17:53:16 +0200

    thanks for oreo

--- src/main/java/swing/content/GridPanel.java
+++ src/main/java/swing/content/GridPanel.java
@@ -7,68 +7,41 @@ import java.util.function.Supplier;
 
 public class GridPanel<C extends Component> extends JPanel {
 
-    private final Class<C> componentClass;
-    private final C[][] components;
-    private final int x, y;
+    private final int xSize, ySize;
+    private final Component[][] components;
+
 
     public GridPanel(int xSize, int ySize, Supplier<Component> componentSupplier) {
-        @SuppressWarnings("unchecked")
-        Class<C> componentSupplierClass = (Class<C>) componentSupplier.get().getClass();
-        this.componentClass = componentSupplierClass;
-        @SuppressWarnings("unchecked")
-        final C[][] instance = (C[][]) Array.newInstance(this.componentClass, xSize, ySize);
-        this.components = instance;
-        this.x = xSize;
-        this.y = ySize;
+        this.xSize = xSize;
+        this.ySize = ySize;
+        this.components = new Component[this.xSize][this.ySize];
+
         this.setLayout(new GridBagLayout());
         GridBagConstraints gbc = new GridBagConstraints();
+
         for (int x = 0; x < xSize; x++) {
             gbc.gridx = x;
             for (int y = 0; y < ySize; y++) {
                 gbc.gridy = y;
-                @SuppressWarnings("unchecked")
-                final C c = (C) componentSupplier.get();
-                this.components[x][y] = c;
+                this.components[x][y] = componentSupplier.get();
                 this.add(this.components[x][y], gbc);
             }
         }
     }
 
     public C componentAt(int x, int y) {
-        return this.components[x][y];
-    }
-
-    public C[] getRow(int row) {
-        C[] array = this.array(this.x);
-        for (int i = 0; i < array.length; i++) {
-            array[i] = this.components[i][row];
-        }
-        return array;
-    }
-
-    public C[] getColumn(int column) {
-        C[] array = this.array(this.y);
-        System.arraycopy(this.components[column], 0, array, 0, array.length);
-        return array;
+        return (C)this.components[x][y];
     }
 
     public C[][] allComponents() {
-        return this.components;
+        return (C[][]) this.components;
     }
 
-    private C[] array(int size) {
-        @SuppressWarnings("unchecked")
-        final C[] array = (C[]) Array.newInstance(this.componentClass, size);
-        return array;
+    public int xSize() {
+        return this.xSize;
     }
 
-    @Override
-    public int getX() {
-        return this.x;
-    }
-
-    @Override
-    public int getY() {
-        return this.y;
+    public int ySize() {
+        return this.ySize;
     }
 }
