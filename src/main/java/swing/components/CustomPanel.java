77623821cbebd531be233b7769f2217bb47c30c7
Lara <69314462+WearBlackAllDay@users.noreply.github.com>
2021-03-01 16:28:09 +0100

    another builder

--- src/main/java/swing/components/CustomPanel.java
+++ src/main/java/swing/components/CustomPanel.java
@@ -12,9 +12,25 @@ import java.util.function.Supplier;
 public class CustomPanel extends JPanel {
 
     private final Map<String, JTextField> textFields = new HashMap<>();
+    private final Map<Key<?>, Component> components = new HashMap<>();
+    private final int defaultWidth, defaultHeight;
+
+    public CustomPanel(LayoutManager layoutManager, int defaultWidth, int defaultHeight) {
+        this.setLayout(layoutManager);
+        this.defaultWidth = defaultWidth;
+        this.defaultHeight = defaultHeight;
+    }
 
     public CustomPanel(LayoutManager layoutManager) {
-        this.setLayout(layoutManager);
+        this(layoutManager, 0, 0);
+    }
+
+    public CustomPanel(int defaultWidth, int defaultHeight) {
+        this(new FlowLayout(), defaultWidth, defaultHeight);
+    }
+
+    public CustomPanel() {
+        this(new FlowLayout());
     }
 
     public CustomPanel addTextField(String text, int width, int height, String id) {
@@ -24,6 +40,10 @@ public class CustomPanel extends JPanel {
         this.add(jTextField);
         this.textFields.put(id, jTextField);
         return this;
+    }
+
+    public CustomPanel addTextField(String text, String id) {
+        return this.addTextField(text, this.defaultWidth, this.defaultHeight, id);
     }
 
     public String getText(String id) {
@@ -42,8 +62,21 @@ public class CustomPanel extends JPanel {
         return this;
     }
 
-    public <C extends Component> CustomPanel addComponent(Supplier<C> cSupplier) {
-        this.add(cSupplier.get());
+    public CustomPanel addButton(String text, ActionListener actionListener) {
+        return this.addButton(text, this.defaultWidth, this.defaultHeight, actionListener);
+    }
+
+    public <C extends Component> CustomPanel addComponent(Key<C> key, Supplier<C> cSupplier) {
+        this.components.put(key, cSupplier.get());
+        this.add(this.getComponent(key));
         return this;
+    }
+
+    @SuppressWarnings("unchecked")
+    public <C extends Component> C getComponent(Key<C> key) {
+        return (C)this.components.get(key);
+    }
+
+    public static class Key<C> {
     }
 }
---
+++ src/main/java/swing/components/MenuBuilder.java
@@ -0,0 +1,95 @@
+package swing.components;
+
+import swing.SwingUtils;
+
+import javax.swing.*;
+import java.awt.event.ActionListener;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.function.Consumer;
+import java.util.function.Supplier;
+
+public class MenuBuilder extends JMenuBar {
+
+    private final Map<Key<?>, JMenuItem> items = new HashMap<>();
+
+    public MenuBuilder addTab(String title, JMenuItem... items) {
+        JMenu jMenu = new JMenu(title);
+        SwingUtils.addSet(jMenu, items);
+        this.add(jMenu);
+        return this;
+    }
+
+    @SuppressWarnings("unchecked")
+    public <C extends JMenuItem> C customItem(Key<C> key, Supplier<C> menuItem) {
+        this.items.put(key, menuItem.get());
+        return (C)this.items.get(key);
+    }
+
+    @SuppressWarnings("unchecked")
+    public <C extends JMenuItem> C getItem(Key<C> key) {
+        return (C)this.items.get(key);
+    }
+
+    public static class Item extends JMenuItem {
+        public Item(String title, ActionListener actionListener) {
+            this.setText(title);
+            this.addActionListener(actionListener);
+        }
+
+        public Item addIcon(Icon icon) {
+            this.setIcon(icon);
+            return this;
+        }
+
+        public Item run(Consumer<JMenuItem> consumer) {
+            consumer.accept(this);
+            return this;
+        }
+    }
+
+    public static class CheckBox extends JCheckBoxMenuItem {
+        public CheckBox(String title, ActionListener actionListener) {
+            this.setText(title);
+            this.addActionListener(actionListener);
+        }
+
+        public CheckBox addIcon(Icon icon) {
+            this.setIcon(icon);
+            return this;
+        }
+
+        public CheckBox run(Consumer<JMenuItem> consumer) {
+            consumer.accept(this);
+            return this;
+        }
+    }
+
+    public static class RadioBox extends JRadioButtonMenuItem {
+        public RadioBox(String title, ActionListener actionListener) {
+            this.setText(title);
+            this.addActionListener(actionListener);
+        }
+
+        public RadioBox addIcon(Icon icon) {
+            this.setIcon(icon);
+            return this;
+        }
+
+        public RadioBox run(Consumer<JMenuItem> consumer) {
+            consumer.accept(this);
+            return this;
+        }
+    }
+
+
+    public static class Menu extends JMenu {
+        public Menu(String title, JMenuItem... items) {
+            this.setText(title);
+            SwingUtils.addSet(this, items);
+        }
+    }
+
+    public static class Key<T> {
+    }
+}
